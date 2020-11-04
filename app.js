8357f49beb0d7c3ad57e1caf29796d45e33d0a66
Ansama <tranminh534@gmail.com>
2020-11-04 13:44:12 +0700

    demo part 3

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var post = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 app.get('/', function(req, res){
 	res.render('index'); 
 });
@@ -14,15 +16,67 @@ app.get('/login', function(req, res){
 	res.render('login'); 
 });
 
+var users = [
+
+			{id: 1, name: "Tran", age: "19"},
+			{id: 2, name: "Hoang", age: "21"},
+			{id: 3, name: "An", age: "22"},
+			{id: 4, name: "Xuan", age: "19"},
+			{id: 5, name: "Khai", age: "21"},
+			{id: 6, name: "Nguyen", age: "22"},
+			{id: 7, name: "Luu", age: "19"},
+			{id: 8, name: "Dung", age: "21"},
+			{id: 9, name: "Chien", age: "22"}
+];
+
 app.get('/user', function(req, res){
 	res.render('users/user', {
-		users: [
-			{id: 1, name: "Hoang", age: "19"},
-			{id: 2, name: "An", age: "21"},
-			{id: 3, name: "Tran", age: "22"}
-		]
+		users: users
 	}); 
 });
+
+app.get('/user/search', function(req, res) {
+	var q = req.query.q;
+	var matchUser = users.filter(function(user) {
+		return user.name.indexOf(q) !== -1;
+	});
+
+	res.render('users/user', {
+		users: matchUser,
+		value : q
+	}); 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(post, () => {
